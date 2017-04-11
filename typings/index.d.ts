c3ea19fc32c79d5d7d76d51f484785b7bc0b6f7a
Your Name <you@example.com>
2017-04-11 12:06:51 +1000

    adding typings to project

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/form-data/index.d.ts" />
+/// <reference path="modules/request-promise-native/index.d.ts" />
+/// <reference path="modules/request/index.d.ts" />
